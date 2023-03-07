484774c63d660ac100dfc2cc6d4cecd99c4b6a1f
Max Voloshinskii <me@voloshinskii.ru>
2023-03-07 07:55:45 +0400

    minor fix

--- src/interpolations/stepInterpolation.js
+++ src/interpolations/stepInterpolation.js
@@ -1,8 +1,8 @@
 export default function stepInterpolation(points) {
     return points.reduce((acc, point, idx) => {
         acc.push(point);
-        if (cachedData[idx + 1]) {
-        acc.push({ x: cachedData[idx + 1].x, y: point.y });
+        if (points[idx + 1]) {
+        acc.push({ x: points[idx + 1].x, y: point.y });
         }
         return acc;
     }, []);
